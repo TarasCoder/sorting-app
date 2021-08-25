b1e8d00c3d1182f6f1ddbbf33c07393c1f441331
kaskad <kaskad@bigmir.net>
2021-08-25 16:21:05 +0300

    correcting API connection

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var xhttp = new XMLHttpRequest();
+let xhttp = new XMLHttpRequest();
 let url = "https://randomuser.me/api?results=50";
 xhttp.open("GET", url, true);
 xhttp.onload = function () {
   let responce = JSON.parse(this.response);
+
   responce.results.forEach((element) => {
     let firstName = element.name.first;
     let lastName = element.name.last;
@@ -10,7 +11,29 @@ xhttp.onload = function () {
     let city = element.location.city;
     let country = element.location.country;
 
-  });
+    let div = document.createElement("div");
+    div.classList.add("textWrap");
 
+    let li = document.createElement("li");
+    li.classList.add("listItem");
+
+    let img = document.createElement("img");
+    img.classList.add("userPicture");
+    img.setAttribute("src", picture);
+
+    let p1 = document.createElement("p");
+    p1.classList.add("userName");
+    p1.innerText = firstName + " " + lastName;
+
+    let p2 = document.createElement("p");
+    p2.classList.add("userLocation");
+    p2.innerText = city + " " + country;
+
+    li.append(img);
+    li.append(div);
+    div.append(p1);
+    div.append(p2);
+    document.querySelector(".list").append(li);
+});
 };
 xhttp.send();
